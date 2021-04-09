75873fdf1721ce57faed2ae8d8cc3f1e3e76ac0d
Enrico <enricorobazzi@gmail.com>
2021-04-09 08:43:44 -0300

    Updated procfile

--- core/asgi.py
+++ core/asgi.py
@@ -1,19 +1,18 @@
 import os
+import django
+
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 
 from django.core.asgi import get_asgi_application
 import main.routing
-import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-django.setup()
-
 application = ProtocolTypeRouter({
-#   "http": get_asgi_application(),
-  "http": get_default_application(),
+  "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             main.routing.websocket_urlpatterns
--- core/settings.py
+++ core/settings.py
@@ -13,13 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
-import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-django.setup()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
