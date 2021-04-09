d15b43a81161acdae3719a1ee82e05e8dbfce073
Enrico <enricorobazzi@gmail.com>
2021-04-08 22:34:50 -0300

    Included django config

--- core/asgi.py
+++ core/asgi.py
@@ -4,8 +4,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import main.routing
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+django.setup()
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
