6aed94cd16ee7c63fc58bac84dd9bfda5f494ad1
Enrico <enricorobazzi@gmail.com>
2021-04-09 08:31:21 -0300

    Trying django setup

--- core/settings.py
+++ core/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
